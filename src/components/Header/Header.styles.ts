2d00e3fa741145024d5781ad1818fc55cdebcd27
YoungCheon Kim <kyt031522@gmail.com>
2025-01-01 12:04:44 +0900

    feat: 헤더 Props방식으로 변경 - 주석 추가

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -2,53 +2,38 @@ import { styled } from "styled-components";
 
 const Header = styled.header`
     position: fixed;
-    display:block;
     top: 0;
     left: 0;
-    background-color: transparent;
     width: 100%;
+    background-color: transparent;
     z-index: 100;
-`
+`;
 
 const HeaderWrapper = styled.div`
-    display:grid;
-    grid-template-columns:repeat(3,1fr);
-    margin: 0 20px;
-    padding-top:0.75rem;
-    padding-bottom:1rem;
-    height: 100%;
-    gap:1rem;
-`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 0.75rem 20px 1rem;
+`;
 
-const Left = styled.div`
-    gap:1rem;
-    display:flex;
-    justify-content:flex-start;
-    align-items:center;
-`
-
-const Center = styled.div`
+const Section = styled.div`
     display: flex;
-    justify-content: center;
     align-items: center;
-    gap:1rem;
-`
-
-const Right = styled.div`
-    display:flex;
-    justify-content:flex-end;
-    align-items:center;
-    gap:1rem;
-`
-
-
-
-
+    
+    &:nth-child(1) {
+        justify-content: flex-start;
+    }
+    
+    &:nth-child(2) {
+        justify-content: center;
+    }
+    
+    &:nth-child(3) {
+        justify-content: flex-end;
+    }
+`;
 
 export {
     Header,
     HeaderWrapper,
-    Left,
-    Center,
-    Right
-}
+    Section
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,32 @@
 import * as S from "./Header.styles";
 
 interface HeaderProps {
-    children?: React.ReactNode;
+    leftContent?: React.ReactNode;
+    centerContent?: React.ReactNode;
+    rightContent?: React.ReactNode;
 }
 
-export function Header({children}:HeaderProps){
-    return(
+/**
+ * 헤더 컴포넌트로 좌측, 우측, 중앙에 각각 콘텐츠를 배치할 수 있습니다.
+ * @param leftContent 왼쪽 컨텐츠
+ * @param centerContent 중앙 컨텐츠
+ * @param rightContent 오른쪽 컨텐츠
+ */
+
+export default function Header({ leftContent, centerContent, rightContent }: HeaderProps) {
+    return (
         <S.Header>
             <S.HeaderWrapper>
-                {children}
+                <S.Section>
+                    {leftContent}
+                </S.Section>
+                <S.Section>
+                    {centerContent}
+                </S.Section>
+                <S.Section>
+                    {rightContent}
+                </S.Section>
             </S.HeaderWrapper>
         </S.Header>
-    )   
-}
-
-Header.leftContent = ({children}:HeaderProps) => {
-    return <S.Left>{children}</S.Left>
-}
-
-Header.centerContent = ({children}:HeaderProps) => {
-    return <S.Center>{children}</S.Center>
-}
-
-Header.rightContent = ({children}:HeaderProps) => {
-    return <S.Right>{children}</S.Right>
+    );
 }

38bc6b586dea98a239061c2be9f58671553aba64
YoungCheon Kim <kyt031522@gmail.com>
2025-01-02 18:37:41 +0900

    feat: 디자인 시스템 적용 - 색상 적용

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        primary: '#3B46F1',
+        secondary: '#222222',
+        background: '#0A0A0A',
+        disabled: '#434343',
+    },
+};
+
+export type Theme = typeof theme;

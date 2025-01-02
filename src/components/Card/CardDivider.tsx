fe99672adf020d6892fa5ff102507928dbc1f491
YoungCheon Kim <kyt031522@gmail.com>
2025-01-02 23:12:44 +0900

    style: card Divider, Item 책임 분리 - divider padding 삭제 - item은 그냥 담는 역할만

--- src/components/Card/CardDivider.tsx
+++ src/components/Card/CardDivider.tsx
@@ -2,9 +2,9 @@ import * as S from "./Card.styles";
 
 export default function CardDivider() {
     return (
-        <S.ItemWrapper> 
+        <> 
             <S.Divider />
-        </S.ItemWrapper>
+        </>
     )
 }
 

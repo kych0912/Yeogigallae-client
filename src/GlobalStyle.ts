fac458ec9efa6f2992a031e838888614685bc702
YoungCheon Kim <kyt031522@gmail.com>
2025-01-08 16:16:35 +0900

    style: 자잘한 수정

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,22 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  html, body {
-    margin: 0;
-    padding: 0;
-    font-family: Arial, sans-serif;
-    background-color: transparent;
-    overflow: hidden;
-    width: 100%;
-    height: 100%;
-  }
-
-  #root {
-    width: 430px; 
-    height: 932px;
-    background-color: ${({ theme }) => theme.colors.background}; 
-    overflow: auto;
-  }
 
   h1 {
     color: ${({ theme }) => theme.colors.primary}; 

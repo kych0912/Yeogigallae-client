a87d3d5e7a4b7ba68196a2547fe0395dafa19bd5
YoungCheon Kim <kyt031522@gmail.com>
2025-01-11 17:50:45 +0900

    fix: 파라미터로 퍼널 관리

--- src/hooks/useFunnel/useFunnel.tsx
+++ src/hooks/useFunnel/useFunnel.tsx
@@ -1,4 +1,5 @@
-import { ReactElement, ReactNode, useState } from 'react';
+import { ReactElement, ReactNode } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export interface StepProps {
   name: string;
@@ -10,18 +11,27 @@ export interface FunnelProps {
 }
 
 export const useFunnel = (defaultStep: string) => {
-  // state를 통해 현재 스텝을 관리한다.
-  // setStep 함수를 통해 현재 스텝을 변경할 수 있다.
-  const [step, setStep] = useState(defaultStep);
 
-  // 각 단계를 나타내는 Step 컴포넌트
-  // children을 통해 각 스텝의 컨텐츠를 렌더링 한다.
+  //searchParams를 통해 현재 스텝을 관리한다.
+  const [searchParams, setSearchParams] = useSearchParams();
+  const step = searchParams.get("step") || defaultStep;
+
+  //Step 컴포넌트는 각 스텝의 컨텐츠를 렌더링한다.
+  //여기서 name을 이용해서 Funnel 컴포넌트에서 현재 스텝을 찾는다.
   const Step = (props: StepProps): ReactElement => {
     return <>{props.children}</>;
   };
 
-  // 여러 단계의 Step 컴포넌트 중 현재 활성화된 스텝을 렌더링하는 Funnel
-  // find를 통해 Step 중 현재 Step을 찾아 렌더링
+  //setStep 함수로 url 파라미터를 변경한다.
+  const setStep = (step: string) => {
+    setSearchParams((prev) => {
+      prev.set("step", step);
+      return prev;
+    });
+  };
+
+  //Funnel 컴포넌트는 여러 단계의 Step 컴포넌트 중 현재 활성화된 스텝을 렌더링한다.
+  //find를 통해 Step 중 현재 Step을 찾아 렌더링
   const Funnel = ({ children }: FunnelProps) => {
     const targetStep = children.find((childStep) => childStep.props.name === step);
 

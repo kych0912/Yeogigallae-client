12c94a704a6f91188cacf2ed23a72aec19c8a111
YoungCheon Kim <kyt031522@gmail.com>
2025-01-12 21:14:57 +0900

    style: 채팅 인터페이스 구현

---
+++ src/pages/course/ShareCourse/Share/_components/InfoToggle.tsx
@@ -0,0 +1,22 @@
+import UpwardTrend from "../../../../../assets/icons/UpwardTrend.svg?react";
+import {CompleteMessage} from "../../../_components/Course.style";
+import Down from "../../../../../assets/icons/Down.svg?react";
+import * as S from "./Style";
+
+export default function InfoToggle(){
+    return(
+        <>
+            <S.Toggle.Wrapper>
+                <UpwardTrend />
+                <S.Toggle.Title>코스짜기 위한 장소 의견 받는중...</S.Toggle.Title>
+                <Down />
+            </S.Toggle.Wrapper>
+
+            <CompleteMessage style={{marginTop:'0.5rem'}}>   
+                규리님이 코스짜기를 시작했습니다. 
+                <br/>
+                6시간 이후 종료 됩니다.
+            </CompleteMessage>
+        </>
+    )
+}

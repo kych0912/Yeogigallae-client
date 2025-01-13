86dd88933e0bf0693f67913f100614f67a018a80
YoungCheon Kim <kyt031522@gmail.com>
2025-01-13 13:29:43 +0900

    feat: CardItem label 다시 부활...

--- src/components/Card/CardItem.tsx
+++ src/components/Card/CardItem.tsx
@@ -2,11 +2,23 @@ import * as S from "./Card.styles";
 
 export default function CardItem({ 
     children,  
-    className
+    className,
+    label
   }: { 
     children: React.ReactNode;
     className?:string;
+    label?:string;
   }) {
+
+    if(label){
+      return(
+        <S.ItemWrapper className={className}>
+          <S.Label>{label}</S.Label>
+          <S.Content>{children}</S.Content>
+        </S.ItemWrapper>
+      )
+    }
+
     return(
         <S.ItemWrapper className={className}>
           {children}

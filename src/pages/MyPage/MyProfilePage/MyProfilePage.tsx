93c63e9d02b8aeaaf35ea17af81df5fe2c4a58a4
YoungCheon Kim <kyt031522@gmail.com>
2025-01-18 21:18:08 +0900

    style:profile setting 마크업

--- src/pages/MyPage/MyProfilePage/MyProfilePage.tsx
+++ src/pages/MyPage/MyProfilePage/MyProfilePage.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import * as S from './_components/Profile.style'; 
 import UserProfile from './_components/UserProfile';
-
+import ServiceSetting from './_components/ServiceSetting';
 const MyProfilePage: React.FC = () => {
     return (
         <S.Container>
             <UserProfile avatar={"https://picsum.photos/200/300"} />
+            <ServiceSetting />
         </S.Container>
     );
 };
--- src/pages/MyPage/MyProfilePage/_components/Profile.style.ts
+++ src/pages/MyPage/MyProfilePage/_components/Profile.style.ts
@@ -45,3 +45,30 @@ export const UserProfile = {
     margin-top:0.25rem;
   `
 }
+
+export const ServiceSetting = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+    width:100%;
+    gap:1.5rem;
+  `,
+  Title: styled.div`
+    font-size:1rem;
+    font-family:${({theme}) => theme.fontFamily.semiBold};
+    color:#ffffff;
+  `,
+  Setting:styled.div`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    width:100%;
+    padding-left:0.875rem;
+    font-family:${({theme}) => theme.fontFamily.regular};
+    color:#ffffff;
+    font-size:0.875rem;
+    line-height:1rem;
+  `
+}

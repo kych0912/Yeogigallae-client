3329d9b74d93f081332ed0ec6b9846380aa4ad67
YoungCheon Kim <kyt031522@gmail.com>
2025-01-20 04:33:02 +0900

    style:공통 container 추출

--- src/pages/MyPage/MyProfilePage/MyProfilePage.tsx
+++ src/pages/MyPage/MyProfilePage/MyProfilePage.tsx
@@ -1,14 +1,16 @@
 // 프로필 화면
 import React from 'react';
-import * as S from './_components/Profile.style'; 
+
 import UserProfile from './_components/UserProfile';
 import ServiceSetting from './_components/ServiceSetting';
+import CommonContainer from '../../../components/Layout/CommonContainer';
+
 const MyProfilePage: React.FC = () => {
     return (
-        <S.Container>
+        <CommonContainer>
             <UserProfile avatar={"https://picsum.photos/200/300"} />
             <ServiceSetting />
-        </S.Container>
+        </CommonContainer>
     );
 };
 

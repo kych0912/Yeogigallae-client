08f1e0529002b9e11df40fbb95c57f281bb10873
YoungCheon Kim <kyt031522@gmail.com>
2025-01-20 14:47:13 +0900

    feat: 친구 페이지 구현

--- src/pages/MyPage/MyFriendPage/MyFriendPage.tsx
+++ src/pages/MyPage/MyFriendPage/MyFriendPage.tsx
@@ -1,12 +1,71 @@
 // 마이페이지
 import React from 'react';
-import * as S from './Styles'; 
+import * as S from './_components/Friend.style'; 
+import CommonContainer from '../../../components/Layout/CommonContainer';
+import {
+    Accordion,
+    AccordionTitle,
+    AccordionItemWrapper,
+    AccordionItem
+} from './_components/Accordion';
 
 const MyFriendPage: React.FC = () => {
+
+
     return (
-        <S.Container>
-            <S.Title>My Friend Page!</S.Title>
-        </S.Container>
+        <CommonContainer>
+            <Accordion>
+                <AccordionTitle>
+                    {"여행 크루 만들기"}
+                </AccordionTitle>
+                <AccordionItemWrapper>
+                    <AccordionItem index={0}>
+                        <S.Item.AddCircle>
+                            {"+"}
+                        </S.Item.AddCircle>
+                        {"친구 추가"}
+                    </AccordionItem>
+                    <AccordionItem index={1}>
+                        <S.Accordion.Avatar src="https://picsum.photos/200/300" alt="friend" />
+                            {"친구 목록"}
+                    </AccordionItem>
+                    <AccordionItem index={2}>
+                        <S.Accordion.Avatar src="https://picsum.photos/200/300" alt="friend" />
+                            {"친구 목록"}
+                    </AccordionItem>
+                    <AccordionItem index={3}>
+                        <S.Accordion.Avatar src="https://picsum.photos/200/300" alt="friend" />
+                            {"친구 목록"}
+                    </AccordionItem>
+                </AccordionItemWrapper>
+            </Accordion>
+
+            <Accordion>
+                <AccordionTitle>
+                    {"여행 크루 만들기"}
+                </AccordionTitle>
+                <AccordionItemWrapper>
+                    <AccordionItem index={0}>
+                        <S.Item.AddCircle>
+                            {"+"}
+                        </S.Item.AddCircle>
+                        {"친구 추가"}
+                    </AccordionItem>
+                    <AccordionItem index={1}>
+                        <S.Accordion.Avatar src="https://picsum.photos/200/300" alt="friend" />
+                            {"친구 목록"}
+                    </AccordionItem>
+                    <AccordionItem index={2}>
+                        <S.Accordion.Avatar src="https://picsum.photos/200/300" alt="friend" />
+                            {"친구 목록"}
+                    </AccordionItem>
+                    <AccordionItem index={3}>
+                        <S.Accordion.Avatar src="https://picsum.photos/200/300" alt="friend" />
+                            {"친구 목록"}
+                    </AccordionItem>
+                </AccordionItemWrapper>
+            </Accordion>
+        </CommonContainer>
     );
 };
 

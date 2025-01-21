1c663ac35eb6f56bdc4c257541c431251c1e8849
YoungCheon Kim <kyt031522@gmail.com>
2025-01-21 17:31:42 +0900

    style: Notice Layout 적용

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import CourseLayout from './components/Layout/CourseLayout';
 import CoursePage from "./pages/course/page";
 import ProfileLayout from "./components/Layout/ProfileLayout";
+import NoticeLayout from "./components/Layout/NoticeLayout";
 
 const queryClient = new QueryClient();
 
@@ -64,7 +65,9 @@ const App: React.FC = () => {
             </Route>
 
             {/* Notice */}
-            <Route path="/notice" element={<NoticePage />} />
+            <Route element={<NoticeLayout />}>
+              <Route path="/notice" element={<NoticePage />} />
+            </Route>
 
             {/* Scheduling */}
             <Route path="/scheduling/budget-select" element={<BudgetSelectPage />} />

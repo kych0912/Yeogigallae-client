b4e49f3fc033ff18b0ad6e6dce2f8ab6dc48ce9f
YoungCheon Kim <kyt031522@gmail.com>
2025-01-25 22:11:37 +0900

    Delete src/pages/course directory

--- src/pages/course/SharedCourse/Overview/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Route } from "../../../../apis/map/types";
-import CourseOverviewCard from "./_components/CourseOverviewCard";
-
-export default function Overview({dailyRoutes,onNext}:{dailyRoutes:Route | null | undefined,onNext:()=>void}){
-    return(
-        <CourseOverviewCard dailyRoutes={dailyRoutes} onNext={onNext} />
-    )
-}

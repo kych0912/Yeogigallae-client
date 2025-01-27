cbc7dd7b771d1d4c5d301f93a3b3289ceb7f6dd0
YoungCheon Kim <kyt031522@gmail.com>
2025-01-28 03:14:26 +0900

    feat:UpComingCourse route 연결

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import MyFriendPage from "./pages/MyPage/MyFriendPage/MyFriendPage";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NoticeLayout from "./components/Layout/NoticeLayout";
+import UpComingCoursePage from "./pages/Course/UpComingCourse/UpComingCoursePage";
 
 const queryClient = new QueryClient();
 
@@ -71,6 +72,7 @@ const App: React.FC = () => {
 
                         <Route element={<CourseLayout />}>
                             <Route path="/course" element={<CoursePage />} />
+                            <Route path="/course/upcoming" element={<UpComingCoursePage />} />
                         </Route>
                     </Routes>
                 </Router>

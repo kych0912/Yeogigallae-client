953f415b3f119d24f3df734849ea35b55acd475c
YoungCheon Kim <kyt031522@gmail.com>
2025-01-28 03:44:03 +0900

    feat: header 설정

--- src/pages/Course/UpComingCourse/UpComingCoursePage.tsx
+++ src/pages/Course/UpComingCourse/UpComingCoursePage.tsx
@@ -3,9 +3,17 @@ import RecommendCard from "./_components/RecommendCard";
 import {sampleData} from "../../Course/test"
 import { useGetAllCourses } from "../../../react-query/queries/queries";
 import { UpComingContainer } from "./_components/UpComingCourse.style";
+import { useOutletContext } from "react-router-dom";
+import { HeaderConfig } from "../../../types/header/header";
+import { useEffect } from "react";
 
 export default function UpComingCoursePage() {
+    const {setHeaderConfig} = useOutletContext<{setHeaderConfig: (config: HeaderConfig) => void}>();
     const allCoursesQueries = useGetAllCourses(sampleData);
+
+    useEffect(() => {
+        setHeaderConfig({title:"지구마블", number:4});
+    }, []);
 
     if(allCoursesQueries[0].isLoading) return <div>Loading...</div>;
 

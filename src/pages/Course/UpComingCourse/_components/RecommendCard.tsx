8ef4bcbd8d80f4a6b3029df85dfc32518cad21e5
YoungCheon Kim <kyt031522@gmail.com>
2025-01-28 03:30:45 +0900

    fix: Recommend card button 이름 변경

--- src/pages/Course/UpComingCourse/_components/RecommendCard.tsx
+++ src/pages/Course/UpComingCourse/_components/RecommendCard.tsx
@@ -9,8 +9,8 @@ export default function RecommendCard() {
                 <S.Recommend.RecommendTitle> {"추천 기능"} </S.Recommend.RecommendTitle>
 
                 <S.Recommend.RecommendButtonWrapper>  
-                    <S.Recommend.RecommendButton>   코스 추천 받기   </S.Recommend.RecommendButton>
-                    <S.Recommend.RecommendButton>   코스 추천 받기   </S.Recommend.RecommendButton>
+                    <S.Recommend.RecommendButton>{"투표 하기"}</S.Recommend.RecommendButton>
+                    <S.Recommend.RecommendButton>{"예산 정하기"}</S.Recommend.RecommendButton>
                 </S.Recommend.RecommendButtonWrapper>
             </S.Recommend.RecommendContainer>
         </Card>

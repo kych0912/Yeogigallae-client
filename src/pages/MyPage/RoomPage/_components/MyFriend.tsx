e34852db775108d2563b9743c38756f6afb8fa4c
YoungCheon Kim <kyt031522@gmail.com>
2025-01-30 22:37:54 +0900

    feat: MyFriend context 연결

--- src/pages/MyPage/RoomPage/_components/MyFriend.tsx
+++ src/pages/MyPage/RoomPage/_components/MyFriend.tsx
@@ -1,40 +1,41 @@
-import { Control } from "react-hook-form";
+import { Control, useController } from "react-hook-form";
 import * as S from "./Room.style";
 import { z } from "zod";
 import { roomSchema } from "../schema";
 import MyFriendListItem from "./MyFriendListItem";
+import { useFriend } from "../../../../contexts/FriendContext";
 
 type RoomFormValues = z.infer<typeof roomSchema>;
 
-export default function RoomFriend({control}:{control:Control<RoomFormValues>}) {
-    const friend = [
-        {
-            id:1,
-            name:"박준호",
-            src:"https://placehold.co/2.5rem",
-            size:"2.5rem"
-        },
-        {
-            id:2,
-            name:"박준호",
-            src:"https://placehold.co/2.5rem",
-            size:"2.5rem"
+export default function MyFriend({control}:{
+    control:Control<RoomFormValues>,
+}) {
+    const { 
+        field:{value,onChange}
+    } = useController({control,name:"roomFriend"});
 
-        }
-    ]
+    const { availableFriends, removeFriend } = useFriend();
 
     return ( 
         <S.MyFriend.MyFriendContainer> 
             <S.RoomPage.RoomFriendTitleWrapper>   
                 <S.RoomPage.RoomFriendTitle>{"여행 친구들"}</S.RoomPage.RoomFriendTitle>
-                <S.RoomPage.RoomFriendNumber>{friend.length}</S.RoomPage.RoomFriendNumber>
+                <S.RoomPage.RoomFriendNumber>{availableFriends.length}</S.RoomPage.RoomFriendNumber>
             </S.RoomPage.RoomFriendTitleWrapper>
 
 
             <S.MyFriend.MyFriendListContainer>
                 {
-                    friend.map((friend)=>(  
-                        <MyFriendListItem key={friend.id} name={friend.name} src={friend.src} />
+                    availableFriends.map((friend)=>(  
+                        <MyFriendListItem 
+                            onClick={()=>{
+                                onChange([...value,friend])
+                                removeFriend(friend)
+                            }}
+                            key={friend.id} 
+                            name={friend.name} 
+                            src={friend.src} 
+                        />
                     ))
                 }
             </S.MyFriend.MyFriendListContainer>

e7cad37e0ff6e44bca327eef62a7fd1b66a74e03
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2025-01-31 01:10:49 +0900

    feat/vote-type

---
+++ src/types/voteTypes/voteTypes.ts
@@ -0,0 +1,76 @@
+export type VoteType = "GOOD" | "BAD"; 
+
+// 주석은 지울 예정
+// POST 투표
+export interface VoteRequest {
+  tripId: number;
+  type: VoteType;
+}
+
+export interface VoteResponse {
+  httpStatus: string;
+  code: string;
+  message: string;
+}
+
+export interface VoteErrorResponse {
+  timestamp: string;
+  httpStatus: string;
+  code: string;
+  path: string;
+  errorMessage: string;
+}
+
+// GET 투표 결과
+export interface VoteResultResponse {
+  httpStatus: string;
+  code: string;
+  message: string;
+  result: {
+    userId: number;
+    userName: string;
+    type: VoteType;
+    count: number;
+  }
+}
+
+// GET 여행 정보 
+export interface VoteTripInfoResponse {
+
+}
+
+// POST 날짜 지정
+export interface VoteDaysResponse {
+
+}
+
+// POST  투표 방 생성하기 
+export interface VoteRoomRequest {
+  tripId: number;
+  masterId: number;
+}
+
+export interface VoteRoomResponse {
+
+}
+
+// 필요 시
+export interface VoteRoomErrorResponse {
+  
+}
+
+// POST 투표 확정 여부 판단
+export interface VoteTripResultRequest {
+  tripId: number;
+  roomId: number;
+  voreRoomId: number;
+}
+
+export interface VoteTripResultResponse {
+
+}
+
+// 필요 시
+export interface VoteTripResultErrorResponse {
+  
+}

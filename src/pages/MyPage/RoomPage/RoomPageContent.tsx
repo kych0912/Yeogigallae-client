6d79313fcd6a392124de3694c112eb9cbd0b11ec
YoungCheon Kim <kyt031522@gmail.com>
2025-01-31 13:47:07 +0900

    chore: default state set 삭제

--- src/pages/MyPage/RoomPage/RoomPageContent.tsx
+++ src/pages/MyPage/RoomPage/RoomPageContent.tsx
@@ -7,26 +7,10 @@ import MyFriend from "./_components/MyFriend";
 import RoomTitleForm from "./_components/RoomTitleForm";
 import * as z from "zod";
 import * as S from "./_components/Room.style";
-import { useFriend } from "../../../contexts/FriendContext";
-import { useEffect } from 'react';
 
 type RoomFormValues = z.infer<typeof roomSchema>;
 
-const DEFAULT_MY_FRIEND = [
-    {
-        id:1,
-        name:"박준호1",
-        src:"https://placehold.co/2.5rem",
-    },
-    {
-        id:2,
-        name:"박준호2",
-        src:"https://placehold.co/2.5rem",
-    }
-]
-
 export default function RoomPageContent() {
-    const { setAvailableFriends } = useFriend();
     const {control,handleSubmit,formState:{errors}} = useForm<RoomFormValues>({
         resolver: zodResolver(roomSchema),
         defaultValues:{
@@ -40,10 +24,6 @@ export default function RoomPageContent() {
         console.log('제출된 데이터:', data);
         // 여기에 API 호출 등의 로직 추가
     };
-
-    useEffect(()=>{
-        setAvailableFriends(DEFAULT_MY_FRIEND)
-    },[])
 
     return (
         <S.RoomForm onSubmit={handleSubmit(onSubmit)}>

fce599f0321af8dd320dba87fe6c271960cc77f9
YoungCheon Kim <kyt031522@gmail.com>
2025-02-05 17:18:06 +0900

    feat: getRooms react-query & api 생성 - 타입 추가 - 목업데이터 추가

---
+++ src/apis/room/mocks.ts
@@ -0,0 +1,17 @@
+export const DEFAULT_ROOMS =  [
+    {
+      "roomId": 1,
+      "roomName": "string",
+      "masterName": "강민규"
+    },
+    {
+      "roomId": 10,
+      "roomName": "string",
+      "masterName": "강민규"
+    },
+    {
+      "roomId": 11,
+      "roomName": "string",
+      "masterName": "강민규"
+    }
+  ]
--- src/apis/room/types.ts
+++ src/apis/room/types.ts
@@ -2,3 +2,9 @@ export interface RoomFormValues {
     roomName:string;
     userIds:number[];
 }
+
+export interface Room {
+    roomId:number;
+    roomName:string;
+    masterName:string;
+}

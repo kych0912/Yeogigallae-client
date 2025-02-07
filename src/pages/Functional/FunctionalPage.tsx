a183fc06b31143e57be618ecc58d923fe9f7a5b1
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2025-02-07 22:24:01 +0900

    fix/funnel
    - vote, functional

--- src/pages/Functional/FunctionalPage.tsx
+++ src/pages/Functional/FunctionalPage.tsx
@@ -6,34 +6,39 @@ import CreateVote from "../Functional/CreateVote/_components/CreateVote";
 import CommonContainer from "../../components/Layout/CommonContainer";
 
 export default function FunctionalFunnel() {
-  const { Funnel, Step, setStep } = useFunnel("생성");
+  const funnelOptions = {
+    steps: ["생성", "캘린더", "주소검색"] as const, 
+    init: { step: "생성", context: {} },
+    stepQueryKey: "step",
+  };
+
+  const [FunnelComponent, setStep, contextMap] = useFunnel(funnelOptions);
 
   // 선택된 장소 이름 상태 관리
   const [selectedPlaceName] = useState<string | null>(null);
 
   return (
     <CommonContainer>
-      <Funnel>
-        <Step name="생성">
+      <FunnelComponent>
+        <FunnelComponent.Step name="생성">
           <CreateVote
-            onCalendar={() => setStep("캘린더")}
-            onSearch={() => setStep("주소검색")}
+            onCalendar={() => setStep("캘린더", contextMap["생성"] || {})}
+            onSearch={() => setStep("주소검색", contextMap["생성"] || {})}
             selectedPlaceName={selectedPlaceName} 
           />
-        </Step>
+        </FunnelComponent.Step>
 
-        <Step name="캘린더">
+        <FunnelComponent.Step name="캘린더">
           <CreateCalendar 
-            onNext={() => {
-              setStep("생성"); 
-            }}/>
-        </Step>
+            onNext={() => setStep("생성", contextMap["캘린더"] || {})} 
+          />
+        </FunnelComponent.Step>
 
-        <Step name="주소검색">
+
+        <FunnelComponent.Step name="주소검색">
           <SearchPage />
-        </Step>
-      </Funnel>
+        </FunnelComponent.Step>
+      </FunnelComponent>
     </CommonContainer>
   );
 };
-

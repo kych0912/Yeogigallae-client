67e6e50f6f60f152092dcf7738b39b0c98d5d20e
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2025-02-09 07:36:23 +0900

    chore/votePage

--- src/pages/Vote/VoteResult/_components/VoteResult.tsx
+++ src/pages/Vote/VoteResult/_components/VoteResult.tsx
@@ -13,7 +13,7 @@ export default function VoteResult({
   
   return (
     <>
-      <ResultCard step={step} onNext={onNext} />
+      <ResultCard step={step} type={type} onNext={onNext} />
       <S.Content>
         {voteData.nickName}님이 여행 투표를 올렸습니다. <br />
         48시간 이후 종료됩니다.

f1a691990dbe57a7a33986113d27098b348b7bd5
YoungCheon Kim <kyt031522@gmail.com>
2025-02-09 13:03:25 +0900

    feat: 링크 복사 모달 추가

--- src/pages/MyPage/MyFriendPage/MyFriendPage.tsx
+++ src/pages/MyPage/MyFriendPage/MyFriendPage.tsx
@@ -4,6 +4,7 @@ import { HeaderConfig } from "../../../types/header/header";
 import { useEffect } from "react";
 import Room from "./_components/Room";
 import Friend from "./_components/Friend";
+import Modal from "../../../components/Modal/core";
 
 export default function MyFriendPage() {
     const {setHeaderConfig} = useOutletContext<{setHeaderConfig: (config: HeaderConfig) => void}>();
@@ -14,6 +15,7 @@ export default function MyFriendPage() {
 
     return (
         <CommonContainer>
+            <Modal />
             <Room />
             <Friend />
         </CommonContainer>

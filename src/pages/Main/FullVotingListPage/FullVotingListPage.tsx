15fb9418ed681dcd51743ea36af14c58cfc93b84
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-02-10 03:37:29 +0900

    feat:추가페이지 api 연결

--- src/pages/Main/FullVotingListPage/FullVotingListPage.tsx
+++ src/pages/Main/FullVotingListPage/FullVotingListPage.tsx
@@ -2,6 +2,7 @@ import * as S from "./Styles";
 import FullVotingItem from "../_components/VotingItem/FullVotingItem";
 import TostModal from "../_components/TostModal/TostModal";
 import { useOutletContext } from "react-router-dom";
+import { useGetVoting } from "../../../react-query/queries/main/Voting/queries";
 
 type LayoutContextType = {
     isModalVisible: boolean;
@@ -11,11 +12,28 @@ type LayoutContextType = {
 };
 
 export default function FullVotingListPage() {
+    const { data: votingRooms = [], isLoading, error } = useGetVoting();
+
+    // 로딩 상태에서 콘솔 로그
+    if (isLoading) {
+        console.log("Loading full voting rooms...");
+    }
+
+    // 에러 상태에서 콘솔 로그
+    if (error) {
+        console.error("Error loading full voting rooms:", error); // 에러 로그 출력
+    }
+
     const { isModalVisible, handleCloseModal, selectedFilter, handleFilterChange } = useOutletContext<LayoutContextType>();
+
+    const outletContext = useOutletContext<LayoutContextType>();
+    console.log("Outlet Context:", outletContext);
 
     return (
         <S.Container>
-            <FullVotingItem selectedFilter={selectedFilter} />
+            {/* votingRooms 데이터를 FullVotingItem에 전달 */}
+            <FullVotingItem rooms={votingRooms ?? []} selectedFilter={selectedFilter} />
+
             <TostModal isVisible={isModalVisible} onClose={handleCloseModal} onFilterChange={handleFilterChange} />
         </S.Container>
     );

4466b94490cb60296e5178d810130523c1812951
YoungCheon Kim <kyt031522@gmail.com>
2025-02-10 17:26:21 +0900

    feat: 삭제 api 연결

--- src/apis/friend/index.ts
+++ src/apis/friend/index.ts
@@ -4,10 +4,19 @@ import { Friend } from "./types";
 
 export const getFriends = async () => {
     if(import.meta.env.MODE === "development"){
-        return DEFAULT_MY_FRIEND;
+        return DEFAULT_MY_FRIEND.filter((item)=> item.friendId != 1);
     }
 
-    const response = await axios.get<Friend[]>(`${import.meta.env.VITE_API_URL}/friend-info`);
+    const response = await axios.get<Friend[]>(`${import.meta.env.VITE_API_URL}/friendship/friends`);
     return response.data;
-
 }
+
+export const deleteFriend = async (friendId:number) => {
+    if(import.meta.env.MODE === "development"){
+        return;
+    }
+    
+    const response = await axios.delete(`${import.meta.env.VITE_API_URL}/friendship/friends/${friendId}`);
+    return response.data;
+}   
+

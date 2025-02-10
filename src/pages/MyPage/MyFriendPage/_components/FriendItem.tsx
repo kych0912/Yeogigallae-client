3d4c443a90bed3fe449851885367f871e4dcee42
YoungCheon Kim <kyt031522@gmail.com>
2025-02-10 16:46:54 +0900

    style: type에 따른 삭제 버튼 렌더링

--- src/pages/MyPage/MyFriendPage/_components/FriendItem.tsx
+++ src/pages/MyPage/MyFriendPage/_components/FriendItem.tsx
@@ -2,22 +2,30 @@ import { AccordionItem } from "./Accordion"
 import * as M from "../../_components/MyPage.style"
 import { Member } from "../../../../apis/room/types"
 import ProfileGroup from "./ProfileGroup"
+import * as S from "./Friend.style"
 
 //접히는 컴포넌트용
 export function FriendItem({
     index,
     name,
-    member
+    member,
+    type="friend"
 }:{
     index:number,
     name:string,
     member:Member[],
+    type?: "room" | "friend"
 }){
     return(
         <AccordionItem index={index}>
-            <ProfileGroup members={member} />
+            <S.Accordion.ProfileWrapper>
+                <ProfileGroup members={member} />
                 {name}
+            </S.Accordion.ProfileWrapper>
+            {type === "friend" && <S.Item.DeleteButton/>}
         </AccordionItem>
+
+
 
 
     )
@@ -27,12 +35,15 @@ export function FriendItem({
 export function AddFriendItem({title, onClick}:{title:string, onClick:()=>void}){
     return(
         <AccordionItem index={0}>
-            <M.AddCircle onClick={onClick}>
+            <S.Accordion.ProfileWrapper>
+                <M.AddCircle onClick={onClick}>
+                    {"+"}
+                </M.AddCircle>
+                {title}
+            </S.Accordion.ProfileWrapper>
 
-                {"+"}
-            </M.AddCircle>
-            {title}
         </AccordionItem>
+
 
     )
 }

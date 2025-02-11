f694a00bb28dd6a766f87fb7ab9d843e74df37b0
YoungCheon Kim <kyt031522@gmail.com>
2025-02-11 19:10:34 +0900

    feat: list funnel useform 설정 - validate check - submit - remove - useFieldArray 사용으로 각 아이템 별 validate check

--- src/pages/Course/ShareCourse/List/index.tsx
+++ src/pages/Course/ShareCourse/List/index.tsx
@@ -2,67 +2,95 @@ import ListCard from "./_components/ListCard";
 import { Button } from "../../../../components/Button";
 import * as S from "./_components/Style";
 import AddPlace from "./_components/AddPlace";
-import { useEffect, useState } from "react";
-import { ShareCourseData, TShareCourseContext } from "../ShareCorsePage";
-import { useOutletContext } from "react-router-dom";
-import { HeaderConfig } from "../../../../types/header/header";
+import { ShareCourseData } from "../ShareCorsePage";
+import useSetHeader from "../../../../hooks/useSetHeader";
 import { createPortal } from "react-dom";
+import { useForm, useFieldArray } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ShareCourseListSchema } from "../schema";
+import * as z from "zod";
+
+export type FormData ={
+    places: ShareCourseData
+}
+
+export const FormDataSchema = z.object({
+    places: ShareCourseListSchema
+})
+
+const DefaultPlace:ShareCourseData[number] = {
+    image: "",
+    place: {
+        placeId: 1,
+        placeName: "",
+        lat: 0,
+        lng: 0,
+    },
+    description: "",
+}
 
 export default function List({
     onNext,
-    context,
 }: {
     onNext:(data:ShareCourseData)=>void
-    context:TShareCourseContext
 }){
+    const {
+        handleSubmit, 
+        control, 
+        formState:{isValid},
+    } = useForm<FormData>({
+        resolver: zodResolver(FormDataSchema),
+        defaultValues:{
+            places: [DefaultPlace]
+        }
+    });
 
-    const [places, setPlaces] = useState<ShareCourseData>(context.코스목록 || [{
-        image: "",
-        place: "",
-        description: "",
-    }]);
+    const {fields, append, remove} = useFieldArray({
+        control,
+        name: "places",
+    });
 
     const handleAddPlace = () => {
-        setPlaces([...places, {
-            image: "",
-            place: "",
-            description: "",
-        }]);
+        append(DefaultPlace);
+    };
+
+    useSetHeader({
+        title:"장소 공유하기",
+    });
+
+    const onSubmit = (data:FormData) => {
+        console.log(data.places);
+        onNext(data.places);
     }
 
-    const {setHeaderConfig} = useOutletContext<{setHeaderConfig: (config: HeaderConfig) => void}>();
-
-    useEffect(()=>{
-        setHeaderConfig({title:"장소 공유하기"});
-    },[]);
-
     return (
-        <>
-
+        <form id={"share-course-list"} onSubmit={handleSubmit(onSubmit)}> 
             <S.ItemContainer> 
-                {places.map((place,index) => (
-                    <ListCard 
-                        key={`place-${index}`}
-                        place={place}
-                        onChange={(updatedPlace) => {
-                            const newPlaces = [...places];
-                            newPlaces[index] = updatedPlace;
-                            setPlaces(newPlaces);
-                        }}
-                    />
-                ))}
-
-
+                {fields.map((field,index) => {
+                    return(
+                        <ListCard 
+                            key={field.id}
+                            index={index}
+                            control={control}
+                            remove={() => remove(index)}
+                        />
+                    )
+                })}
                 <AddPlace onClick={handleAddPlace}/>
             </S.ItemContainer>
             {createPortal(
             <S.BottomButtonWrapper>
-                <Button variant="contained" size="large" onClick={()=>onNext(places)}>
+                <Button 
+                    variant="contained" 
+                    size="large"    
+                    type="submit"
+                    form={"share-course-list"}
+                    disabled={!isValid}
+                >
                     {"장소 공유하기"}
                     </Button>
                 </S.BottomButtonWrapper>
             ,document.body)}
-        </>
-
+        </form>
     )
 }

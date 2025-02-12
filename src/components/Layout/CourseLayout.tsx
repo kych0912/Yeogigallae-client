1291804d1e09216b1a0e948c20b1c8e18edb7ed6
YoungCheon Kim <kyt031522@gmail.com>
2025-02-13 03:59:31 +0900

    refactor: setHeader custom hook으로 분리

--- src/components/Layout/CourseLayout.tsx
+++ src/components/Layout/CourseLayout.tsx
@@ -2,25 +2,20 @@ import Header from "../Header";
 import HomeIcon from "../../assets/icons/Home.svg?react";
 import BackIcon from "../../assets/icons/Back.svg?react";
 import { IconButton } from "../Button";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import HeaderCenterContent from "../Header/HeaderCenterContent";
-
-interface HeaderConfig {
-    title:string;
-    number?:number;
-}
+import { HeaderConfig } from "../../types/header/header";
 
 export default function Layout() {
-    const navigate = useNavigate();
     const [headerConfig, setHeaderConfig] = useState<HeaderConfig>({title:""});
-    const { title, number } = headerConfig;
+    const { title, number, leftFunction, rightFunction } = headerConfig;
 
     return (
         <>
             <Header
                 leftContent={
-                    <IconButton onClick={() => navigate(-1)}>
+                    <IconButton onClick={leftFunction}>
                         <BackIcon />
                     </IconButton>
                 }
@@ -28,7 +23,7 @@ export default function Layout() {
                     <HeaderCenterContent title={title} number={number} />
                 }
                 rightContent={
-                    <IconButton onClick={() => navigate("/")}>
+                    <IconButton onClick={rightFunction}>
                         <HomeIcon />
                     </IconButton>
                 }   
--- src/hooks/useSetHeader.ts
+++ src/hooks/useSetHeader.ts
@@ -1,14 +1,23 @@
 import { useOutletContext } from "react-router-dom";
 import { useEffect } from "react";
 import { HeaderConfig } from "../types/header/header";
+import { useNavigate } from "react-router-dom";
 
 export default function useSetHeader({
     title,
     number,
+    leftFunction,
+    rightFunction,
 }:HeaderConfig){
     const {setHeaderConfig} = useOutletContext<{setHeaderConfig: (config: HeaderConfig) => void}>();
-    
+    const navigate = useNavigate();
+
     useEffect(()=>{
-        setHeaderConfig({title:title,number:number});
-    },[title,number,setHeaderConfig]);
+        setHeaderConfig({
+            title:title,
+            number:number,
+            leftFunction:leftFunction || (()=>navigate(-1)),
+            rightFunction:rightFunction || (()=>navigate("/")),
+        });
+    },[title,number,leftFunction,rightFunction,setHeaderConfig,navigate]);
 }   

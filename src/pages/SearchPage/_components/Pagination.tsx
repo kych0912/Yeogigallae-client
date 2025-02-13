336762f4d14cd9baea56db35714640cf783e900d
YoungCheon Kim <kyt031522@gmail.com>
2025-02-13 14:59:01 +0900

    fix: SearchPage  전 버전으로 되돌림

--- src/pages/SearchPage/_components/Pagination.tsx
+++ src/pages/SearchPage/_components/Pagination.tsx
@@ -1,15 +1,20 @@
 import * as S from "./Pagination.styles";
-import { useSearch } from "../context/SearchContext";
+import { PaginationProps } from "../types/types";
 
-export default function Pagination() {
-  const { currentPage, totalPages, setCurrentPage } = useSearch();
+export default function Pagination({
+  currentPage,
+  totalItems,
+  itemsPerPage,
+  onPageChange,
+}: PaginationProps) {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   const handlePrevious = () => {
-    if (currentPage > 1) setCurrentPage(currentPage - 1);
+    if (currentPage > 1) onPageChange(currentPage - 1);
   };
 
   const handleNext = () => {
-    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+    if (currentPage < totalPages) onPageChange(currentPage + 1);
   };
 
   if (totalPages === 0) return null;
@@ -19,7 +24,7 @@ export default function Pagination() {
       <S.ArrowButton onClick={handlePrevious} $disabled={currentPage === 1}>
         {"<"}
       </S.ArrowButton>
-      <S.PageIndicator $isActive>{currentPage}</S.PageIndicator>
+      <S.PageIndicator $isActive={true}>{currentPage}</S.PageIndicator>
       <S.Separator>/</S.Separator>
       <S.PageIndicator $isActive={false}>{totalPages}</S.PageIndicator>
       <S.ArrowButton onClick={handleNext} $disabled={currentPage === totalPages}>
--- src/pages/SearchPage/_components/SearchListItem.tsx
+++ src/pages/SearchPage/_components/SearchListItem.tsx
@@ -3,19 +3,15 @@ import { KakaoPlaceDocument } from "../../../apis/searchAddress/types";
 import * as S from "./ResultList.styles";
 import ToggleIcon from "../../../assets/icons/ToggleIcon.svg?react";
 import MapComponent from "./SearchMap/SearchMap";
-import { useSearch } from "../context/SearchContext";
 
 interface SearchListItemProps {
-  result: KakaoPlaceDocument;
-  index: number;
-  results: KakaoPlaceDocument[];
-  zoneNo: string | undefined;
+    result: KakaoPlaceDocument;
+    index: number;
+    results: KakaoPlaceDocument[];
+    zoneNo: string | undefined;
+    handleSelectItem: (item: KakaoPlaceDocument) => void;
 }
 
-export default function SearchListItem({ result, index, results, zoneNo }: SearchListItemProps) {
-  const [isMapOpen, setIsMapOpen] = useState(false);
-  const navigate = useNavigate();
-  const { selectPlace, centerCoords } = useSearch();
 
 export default function SearchListItem({
     result,
@@ -34,19 +30,34 @@ export default function SearchListItem({
             <S.ResultItem
               $isFirst={index === 0}
               $isLast={index === results.length - 1}
-
             >
-              지도
-              <S.RotateIcon $isRotated={isMapOpen}>
-                <ToggleIcon />
-              </S.RotateIcon>
-            </S.MapButton>
-          </S.InfoContainer>
-        </S.ResultItem>
-      </S.ResultWrapper>
+              <S.ZipCode>{zoneNo || "우편번호 없음"}</S.ZipCode>
+              <S.PlaceName>{result.place_name}</S.PlaceName>
+              <S.InfoContainer>
+                <S.AddressName>
 
-      {isMapOpen && centerCoords && (
+                  {result.road_address_name || result.address_name}
+                </S.AddressName>
+                <S.MapButton onClick={(e) => {
+                    e.stopPropagation();
+                    setIsMapOpen(!isMapOpen)
+                }}>
+                  지도
+                  <S.RotateIcon $isRotated={isMapOpen}>
+                    <ToggleIcon />
+                  </S.RotateIcon>
+                </S.MapButton>
+              </S.InfoContainer>
+            </S.ResultItem>
+          </S.ResultWrapper>
+
+      {isMapOpen && (
         <MapComponent
+          center={{
+            x: result.x.toString(),
+            y: result.y.toString(),
+          }}
+          results={[{ ...result, x: result.x.toString(), y: result.y.toString() }]}
           mapContainerId={`map-${result.id}`}
         />
       )}

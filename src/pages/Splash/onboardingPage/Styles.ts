1f79cfbfc7dfe409fa44be3d901b4f63ef46dc2d
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-02-14 02:11:57 +0900

    style:splash onboarding 연결

--- src/pages/Splash/onboardingPage/Styles.ts
+++ src/pages/Splash/onboardingPage/Styles.ts
@@ -1,49 +1,85 @@
+import BaseButton from "../../../components/Button/Button";
 import styled, { keyframes } from "styled-components";
 
-const dropBounce = keyframes`
-    0% {
-        transform: translateY(-400px);
-        opacity: 0.3;
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+        transform: translateY(10px);
     }
-    50% {
+    to {
+        opacity: 1;
         transform: translateY(0);
-        opacity: 1;
-    }
-    70% {
-        transform: translateY(-50px);
-        opacity: 1;
-    }
-    100% {
-        transform: translateY(0);
-        opacity: 1;
-    }
-`;
-
-export const pulse = keyframes`
-    0%, 100% {
-        transform: scale(1);
-    }
-    50% {
-        transform: scale(1.1);
     }
 `;
 
 export const Container = styled.div`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    width: 100%;
     height: 100vh;
-    background-color: ${({ theme }) => theme.backgroundColor || "#0A0A0A"};
+    background-color: ${({ theme }) => theme.colors.background || "#f0f0f0"};
+    overflow: hidden;
+    box-sizing: border-box;
 `;
 
-export const Logo = styled.img`
-    width: auto;
-    height: 9rem;
-    animation: ${dropBounce} 1s ease-out, ${pulse} 2s infinite 1.5s;
+export const Image = styled.img`
+    width: 100%;
+    margin-bottom: 2.813rem;
+    animation: ${fadeIn} 0.5s ease-in-out;
 `;
 
-export const Title = styled.h1`
-    font-size: 2rem;
-    color: #333;
-    margin-top: 1rem;
+export const Text = styled.div`
+    font-size: 1.75rem;
+    text-align: center;
+    color: white;
+    font-family: ${({ theme }) => theme.fontFamily.semiBold};
+    line-height: 1.5;
+    margin-bottom: 4.625rem;
+    animation: ${fadeIn} 0.5s ease-in-out;
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0 1.25rem;
+`;
+
+export const Button = styled.button`
+    padding: 0 2.5rem;
+    font-size: 1rem;
+    border: none;
+    cursor: pointer;
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.GrayText};
+    font-family: ${({ theme }) => theme.fontFamily.regular};
+`;
+
+export const NextButton = styled(Button)`
+    color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const LButton = styled(BaseButton)`
+    color: white;
+    background-color: ${({ theme }) => theme.colors.primary};
+    width: 100%;
+    margin: 0 1.25rem;
+`;
+
+export const PaginationContainer = styled.div`
+    display: flex;
+    gap: 0.5rem;
+    position: absolute;
+    top: 5%;
+`;
+
+export const Dot = styled.div<{ active: boolean }>`
+    width: ${({ active }) => (active ? "20px" : "10px")};
+    height: 10px;
+    border-radius: 999px;
+    background: ${({ active }) => (active ? ({ theme }) => theme.colors.primary : ({ theme }) => theme.colors.GrayText)};
+    transition: width 0.3s ease-in-out;
+    align-self: center;
 `;
--- src/pages/Splash/onboardingPage/onboardingPagePage.tsx
+++ src/pages/Splash/onboardingPage/onboardingPagePage.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
 import Onboarding1 from "../../../assets/icons/onboarding1.svg";
 import Onboarding2 from "../../../assets/icons/onboarding2.svg";
 import Onboarding3 from "../../../assets/icons/onboarding3.svg";
-
-import BaseButton from "../../../components/Button/Button";
+import * as S from "./Styles";
 
 const images = [Onboarding1, Onboarding2, Onboarding3];
 const texts = [
@@ -14,121 +12,47 @@ const texts = [
     ["짜여진 코스를 바탕으로", "AI가 예상 예산을 알려줘요!"],
 ];
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
-    background-color: ${({ theme }) => theme.colors.background || "#f0f0f0"};
-    overflow-x: hidden;
-    box-sizing: border-box;
-`;
-
-const Image = styled.img<{ marginBottom?: string }>`
-    width: 100%;
-    margin-bottom: ${({ marginBottom }) => marginBottom}; // 기본 값은 2.5rem
-`;
-
-const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    margin-bottom: 5rem;
-    justify-content: space-between;
-    padding: 0 1.25rem;
-`;
-
-const Button = styled.button`
-    padding: 0 2.5rem;
-    font-size: 1rem;
-    border: none;
-    cursor: pointer;
-    background-color: ${({ theme }) => theme.colors.background};
-    color: ${({ theme }) => theme.colors.GrayText};
-    font-family: ${({ theme }) => theme.fontFamily.regular};
-`;
-
-const NextButton = styled(Button)`
-    color: ${({ theme }) => theme.colors.primary};
-`;
-
-const Text = styled.div`
-    font-size: 1.75rem;
-    text-align: center;
-    color: white;
-    font-family: ${({ theme }) => theme.fontFamily.semiBold};
-    line-height: 1.5;
-    margin-bottom: 4.625rem;
-`;
-
-const LButton = styled(BaseButton)`
-    color: white;
-    background-color: ${({ theme }) => theme.colors.primary};
-    width: 100%;
-    margin: 0 1.25rem;
-`;
-
-const PaginationContainer = styled.div`
-    display: flex;
-    gap: 0.5rem;
-    position: absolute;
-    top: 5%;
-`;
-
-const Dot = styled.div<{ active: boolean }>`
-    width: ${({ active }) => (active ? "20px" : "10px")};
-    height: 10px;
-    border-radius: 999px;
-    background: ${({ active }) => (active ? ({ theme }) => theme.colors.primary : ({ theme }) => theme.colors.GrayText)};
-    transition: width 0.3s ease-in-out;
-    align-self: center;
-`;
-
 const Onboarding = () => {
     const [page, setPage] = useState(0);
     const navigate = useNavigate();
 
     const handleNext = () => {
-        setPage((prev) => prev + 1);
+        setPage((prev) => Math.min(prev + 1, images.length - 1));
     };
 
     const handleSkip = () => {
         navigate("/login");
     };
 
-    // 마진값 설정
-    const imageMarginBottom = page === 1 ? "2.5rem" : "0";
-
     return (
-        <Container>
-            <PaginationContainer>
+        <S.Container>
+            <S.PaginationContainer>
                 {images.map((_, index) => (
-                    <Dot key={index} active={index === page} />
+                    <S.Dot key={index} active={index === page} />
                 ))}
-            </PaginationContainer>
+            </S.PaginationContainer>
 
-            <Image src={images[page]} alt={`Page ${page + 1}`} marginBottom={imageMarginBottom} />
-            <Text>
+            <S.Image key={page} src={images[page]} alt={`Page ${page + 1}`} />
+            <S.Text key={`text-${page}`}>
                 {texts[page].map((line, index) => (
                     <span key={index}>
                         {line}
                         {index !== texts[page].length - 1 && <br />}
                     </span>
                 ))}
-            </Text>
-            <ButtonContainer>
+            </S.Text>
+
+            <S.ButtonContainer>
                 {page < images.length - 1 ? (
                     <>
-                        <Button onClick={handleSkip}>건너뛰기</Button>
-                        <NextButton onClick={handleNext}>다음</NextButton>
+                        <S.Button onClick={handleSkip}>건너뛰기</S.Button>
+                        <S.NextButton onClick={handleNext}>다음</S.NextButton>
                     </>
                 ) : (
-                    <LButton onClick={handleSkip}>시작하기</LButton>
+                    <S.LButton onClick={handleSkip}>시작하기</S.LButton>
                 )}
-            </ButtonContainer>
-        </Container>
+            </S.ButtonContainer>
+        </S.Container>
     );
 };
 

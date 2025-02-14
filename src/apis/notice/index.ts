84197e13201e3b2d6482c147ff7fa327a854a7df
YoungCheon Kim <kyt031522@gmail.com>
2025-02-14 14:35:13 +0900

    fix: CORS 에러 시 목업데이터 호출

--- src/apis/notice/index.ts
+++ src/apis/notice/index.ts
@@ -7,7 +7,12 @@ export const getNotice = async (): Promise<Notice[]> => {
         return NoticeMocks;
     }
 
-    const response = await axios.get<Notice[]>(`${import.meta.env.VITE_API_URL}/api/notifications`);
-    return response.data;
+    try{
+        const response = await axios.get<Notice[]>(`${import.meta.env.VITE_API_URL}/api/notifications`);
+        return response.data;
+    }catch(error){
+        console.error("Notice API 호출 오류:", error);
+        return NoticeMocks;
+    }
 };
 

125558424aa359bc9d312cc945906e792a0318d9
YoungCheon Kim <kyt031522@gmail.com>
2025-02-16 02:35:43 +0900

    fix: course api error 시 목업데이터 반환

--- src/apis/course/index.ts
+++ src/apis/course/index.ts
@@ -5,14 +5,14 @@ import * as z from "zod";
 import { ICourseMessageResponse } from "./types";
 export type TShareCoursePlace = z.infer<typeof ShareCourseSchema>["place"];
 
-export const getCourseInfo = async (tripId:string,roomId:string) => {
-    if(import.meta.env.MODE === "development"){
-        await new Promise(resolve => setTimeout(resolve, 3000));
-        return courseInfoMock;
-    }
 
+export const getCourseInfo = async (tripId:string,roomId:string) => {
     try{
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/vote/trip-info/tripId=${tripId}?roomId=${roomId}`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/vote/trip-info?tripId=${tripId}&roomId=${roomId}`,
+            {
+                withCredentials:true,
+            }   
+        );
         return response.data;
     }catch(error){
         console.error("CourseInfo API 호출 오류:", error);
@@ -20,13 +20,12 @@ export const getCourseInfo = async (tripId:string,roomId:string) => {
     }
 }
 
+// 공유된 코스 목록 조회
 export const getCourseList = async (tripId:string,roomId:string):Promise<ICourseMessageResponse> => {
-    if(import.meta.env.MODE === "development"){
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        return courseListMock;
-    }
     try{    
-        const response = await axios.get<ICourseMessageResponse>(`${import.meta.env.VITE_API_URL}/api/rooms/${roomId}/places/${tripId}`);
+        const response = await axios.get<ICourseMessageResponse>(`${import.meta.env.VITE_API_URL}/api/room/${roomId}/places/${tripId}`,{
+            withCredentials:true,
+        });
         return response.data;
     }catch(error){
         console.error("CourseList API 호출 오류:", error);
@@ -34,21 +33,28 @@ export const getCourseList = async (tripId:string,roomId:string):Promise<ICourse
     }
 }
 
+// 공유된 코스 장소 추가
 export const postCoursePlace = async (place:TShareCoursePlace[], roomId:string) => {
-    if(import.meta.env.MODE === "development"){
-        await new Promise(resolve => setTimeout(resolve, 3000));
-        return courseInfoMock;
+    try{
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/room/${roomId}/places`,place,{
+            withCredentials:true,
+        });
+        return response.data;
+    }catch(error){
+        console.error("CoursePlace API 호출 오류:", error);
+        return null;
     }
-    const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/rooms/${roomId}/places`,place);
-    return response.data;
 }
 
 export const deleteCoursePlace = async (id:string, roomId:string) => {
-    if(import.meta.env.MODE === "development"){
-        await new Promise(resolve => setTimeout(resolve, 3000));
-        return courseInfoMock;
+    try{
+        const response = await axios.delete(`${import.meta.env.VITE_API_URL}/api/rooms/${roomId}/places/${id}`,{
+            withCredentials:true,
+        });
+        return response.data;
+    }catch(error){
+        console.error("CoursePlace API 호출 오류:", error);
+        return null;
     }
-    const response = await axios.delete(`${import.meta.env.VITE_API_URL}/api/rooms/${roomId}/places/${id}`);
-    return response.data;
 }
 

e86211bfe2e347839e31f1a450d29c8dacbf9a98
YoungCheon Kim <kyt031522@gmail.com>
2025-02-15 21:30:06 +0900

    fix: api route 연결

--- src/apis/notice/index.ts
+++ src/apis/notice/index.ts
@@ -1,13 +1,14 @@
 import { Notice, NoticeResponse } from "./types";
-import axios from "axios";
 import { NoticeMocks } from "./mocks";
+import axios from "axios";
 
 export const getNotice = async (): Promise<Notice[]> => {
-    if(import.meta.env.MODE === "development"){
+    try{
+        const response = await axios.get<NoticeResponse>(`${import.meta.env.VITE_API_URL}/api/notifications`);
+        return response.data.result;
+    }catch(error){
+        console.error("Notice API 호출 오류:", error);
         return NoticeMocks;
     }
-
-    const response = await axios.get<NoticeResponse>("/api/notifications");
-    return response.data.result;
 };
 

ed25d1a047f6991048356f47b61ace93628bfa60
YoungCheon Kim <kyt031522@gmail.com>
2025-02-16 02:57:36 +0900

    fix:kakao login 방식 변경에 따른 로직 수정 - 유저 정보 zustand에 저장 - api redirectUri 추가

--- src/pages/Login/Kakao.tsx
+++ src/pages/Login/Kakao.tsx
@@ -1,41 +1,40 @@
-// import { useEffect } from "react";
-// import { useNavigate, useSearchParams } from "react-router-dom";
-// import { useAuthStore } from "./useAuthStore";
-// import { sendAuthCodeToServer } from "../../apis/Login/api";
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useAuthStore } from "./useAuthStore";
+import { sendAuthCodeToServer } from "../../apis/Login/api";
 import * as S from "./LoginPage/Styles";
 
 export default function Kakao() {
-    // const navigate = useNavigate();
-    // const [searchParams] = useSearchParams();
-    // const { setAccessToken, setRefreshToken } = useAuthStore();
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const { setProfile } = useAuthStore();
 
-    // useEffect(() => {
-    //     const code = searchParams.get("code");
+    useEffect(() => {
+        const code = searchParams.get("code");
 
-    //     const handleLogin = async () => {
-    //         if (!code) {
-    //             navigate("/login", { replace: true });
-    //             return;
-    //         }
+        const handleLogin = async () => {
+            if (!code) {
+                navigate("/login", { replace: true });
+                return;
+            }
 
-    //         try {
-    //             const response = await sendAuthCodeToServer(code);
+            try {
+                const response = await sendAuthCodeToServer(code);
+                const {success,email,nickname,profileImage} = response;
+                if (success) {
+                    setProfile(email,nickname,profileImage);
+                    navigate("/");
+                } else {
+                    navigate("/login", { replace: true });
+                }
+            } catch (error) {
+                console.error("Login failed:", error);
+                navigate("/login", { replace: true });
+            }
+        };
 
-    //             if (response.accessToken && response.refreshToken) {
-    //                 setAccessToken(response.accessToken);
-    //                 setRefreshToken(response.refreshToken); // refreshToken 저장
-    //                 navigate("/");
-    //             } else {
-    //                 navigate("/login", { replace: true });
-    //             }
-    //         } catch (error) {
-    //             console.error("Login failed:", error);
-    //             navigate("/login", { replace: true });
-    //         }
-    //     };
-
-    //     handleLogin();
-    // }, [searchParams, navigate, setAccessToken, setRefreshToken]);
+        handleLogin();
+    }, [searchParams, navigate,setProfile]);
 
     return (
         <S.Container>

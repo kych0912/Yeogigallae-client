80f3b41161e1e50116b5a822da1655a876d9b666
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2025-02-17 01:24:29 +0900

    fix/apis-file

--- src/apis/vote/tripInfo.ts
+++ src/apis/vote/tripInfo.ts
@@ -5,20 +5,18 @@ import { TripInfoSchema } from "../../pages/Vote/context/tripInfo/tripInfoSchema
 const API_URL = import.meta.env.VITE_API_URL as string;
 
 export const getTripInfo = async (tripId: number, roomId: number) => {
-  // if (import.meta.env.MODE === "development") {
-  //   console.log(DEFAULT_TripInfo);
-  //   return TripInfoSchema.parse(DEFAULT_TripInfo);
-  // }
-
   try {
     const response = await axios.get(
-      `${API_URL}/api/vote/trip-info?tripId=${tripId}&roomId=${roomId}`
+      `${API_URL}/api/vote/trip-info?tripId=${tripId}&roomId=${roomId}`,
+      {
+        withCredentials: true, 
+      }
     );
 
     console.log(response.data);
     return TripInfoSchema.parse(response.data);
   } catch (error) {
-    console.error( error);
+    console.error(error);
 
     return TripInfoSchema.parse(DEFAULT_TripInfo);
   }

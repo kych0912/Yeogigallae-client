0291391794d5b6c187b2986ab4d79f3fee81a6bd
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2025-02-16 14:53:59 +0900

    fix/vote-file
    - 목데이터 변경
    - 안쓰는 파일 삭제

--- src/apis/vote/mocks/voteResultMocks.ts
+++ src/apis/vote/mocks/voteResultMocks.ts
@@ -1,11 +1,37 @@
 export const DEFAULT_VoteResult = {
-  httpStatus: "200 OK", 
+  httpStatus: "200 OK",
   code: "SUCCESS",
-  message: "Vote result fetched successfully", 
-  result: {
-    userId: 900,
-    userName: "고민균",
-    type: "BAD",
-    count: 5,
-  },
+  message: "Vote result fetched successfully",
+  result: [
+    {
+      userId: 101,
+      userName: "김철수",
+      type: "GOOD",
+      count: 2, 
+    },
+    {
+      userId: 102,
+      userName: "이영희",
+      type: "GOOD",
+      count: 2,
+    },
+    {
+      userId: 900,
+      userName: "고민균",
+      type: "BAD",
+      count: 5,
+    },
+    {
+      userId: 201,
+      userName: "박민수",
+      type: "BAD",
+      count: 5,
+    },
+    {
+      userId: 202,
+      userName: "정수진",
+      type: "BAD",
+      count: 5,
+    }
+  ],
 };

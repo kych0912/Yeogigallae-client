482bef05d84a0f18f38609076b30d83d28c9d791
YoungCheon Kim <kyt031522@gmail.com>
2025-02-17 19:03:10 +0900

    feat: hoc 유저 처리 확인 - 로그인 안했을 경우 메인페이지만 목데이터만 보여줌 - 유저 명 목데이터로 - 나머지 페이지는 에러 모달
    - 다른 페이지에서 로그인이 안되어 있을경우
    - 유저 정보 호출 api
    
    - 목데이터 반환 경우 목데이터 유저 정보 설정
    - 아닐경우 에러 모달

--- src/pages/Login/withAuth.tsx
+++ src/pages/Login/withAuth.tsx
@@ -4,6 +4,7 @@ import { useAuthStore } from "./useAuthStore";
 import { getUser } from "../../apis/user";
 import Modal from "../../components/Modal/core";
 import modal from "../../components/Modal";
+import axiosError from "axios";
 import { DEFAULT_User } from "../../apis/user/mocks";
 type AuthOption = true | false | null;
 
@@ -17,57 +18,72 @@ const withAuth = (WrappedComponent: React.ComponentType, authOption: AuthOption,
 
         useEffect(() => {
             const fetchUser = async () => {
+ 
                 //null: 로그인 여부 상관 없이 접근, true: 로그인O 접근, false: 로그인X 접근 가능
                 if (authOption === null) {
                     return;
                 }
 
-                //만약 전역 상태에 유저 정보가 없다면 유저 정보를 가져온다.
-                if (!isAuthenticated) {
-                    try {
-                        const user = isNeedMock ? DEFAULT_User : await getUser();
-                        setProfile(
-                            user.userId,
-                            user.email,
-                            user.nickname,
-                            user.profileImage
-                        );
-                    } catch (error) {
-                        console.log("error:", error);
-                        if (authOption === true) {
-                            modal.confirm.show({
-                                message: "로그인이 필요한 서비스입니다.",
-                                confirmText: "로그인",
-                                cancelText: "취소",
-                                onConfirm: () => {
-                                    navigate('/login');
-                                },
-                                onCancel: () => {
-                                    navigate(-1);
+                if(authOption === false){
+                    return;
+                }
+
+                if(authOption === true){
+                    //만약 전역 상태에 유저 정보가 없다면 유저 정보를 가져온다.
+                    if (!isAuthenticated) {
+                        try {
+                            //만약 로그인 안 되어 있을 때 400 에러 발생
+                            const user = await getUser();
+                            setProfile(
+                                user.userId,
+                                user.email,
+                                user.nickname,
+                                user.profileImage
+                            );
+                        } catch (error) {
+                            if(axiosError.isAxiosError(error) && error.response?.status === 400){
+
+                                //목데이터 테스트 시 목데이터 반환
+                                if(isNeedMock){
+                                    setProfile(
+                                        DEFAULT_User.userId,
+                                        DEFAULT_User.email,
+                                        DEFAULT_User.nickname,
+                                        DEFAULT_User.profileImage
+                                    );
+                                    return;
                                 }
-                            });
-                            return; // 모달 표시 후 추가 실행 방지
+
+                                //목데이터 테스트 아닐 경우 에러 모달
+                                modal.confirm.show({
+                                    message: "로그인이 필요한 서비스입니다.",
+                                    confirmText: "로그인",
+                                    cancelText: "취소",
+                                    onConfirm: () => {
+                                        navigate('/login');
+                                    },
+                                    onCancel: () => {
+                                        navigate(-1);
+                                    }
+                                });
+                            }
+                            if (authOption === true) {
+                                modal.confirm.show({
+                                    message: "로그인이 필요한 서비스입니다.",
+                                    confirmText: "로그인",
+                                    cancelText: "취소",
+                                    onConfirm: () => {
+                                        navigate('/login');
+                                    },
+                                    onCancel: () => {
+                                        navigate(-1);
+                                    }
+                                });
+                                return; // 모달 표시 후 추가 실행 방지
+                            }
                         }
                     }
-                }
 
-                // 로그인 필요 서비스 접근 시 모달 표시
-                if (authOption === true && !isAuthenticated && !isNeedMock) {
-                    modal.confirm.show({
-                        message: "로그인 후 이용해주세요.",
-                        confirmText: "로그인",
-                        cancelText: "취소",
-                        onConfirm: () => {
-                            navigate('/login');
-                        },
-                        onCancel: () => {
-                            navigate(-1);
-                        }
-                    });
-                    return; // 모달 표시 후 추가 실행 방지
-                }
-
-                if (authOption === false && isAuthenticated) {
                     return;
                 }
             };

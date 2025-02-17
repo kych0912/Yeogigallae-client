8b8f95311488882357f077aad4e9db96773a4667
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-02-17 14:36:13 +0900

    fix:$추가

--- src/pages/Main/_components/FloatingButton/FloatingButton.tsx
+++ src/pages/Main/_components/FloatingButton/FloatingButton.tsx
@@ -24,22 +24,22 @@ export default function FloatingMenu() {
 
     return (
         <S.FloatingContainer>
-            <S.Overlay isVisible={isVisible} onClick={closeMenu} />
+            <S.Overlay $isVisible={isVisible} onClick={closeMenu} />
 
-            <S.FloatingButtonStyled isMain isVisible={isVisible} onClick={toggleMenu}>
+            <S.FloatingButtonStyled $isMain $isVisible={isVisible} onClick={toggleMenu}>
                 <img src={Floating} alt="Floating Icon" />
             </S.FloatingButtonStyled>
 
-            <S.MenuItem isVisible={isVisible}>
-                <S.SubText isVisible={isVisible}>여행 친구 관리</S.SubText>
-                <S.SubButton isVisible={isVisible} onClick={handleMyClick}>
+            <S.MenuItem $isVisible={isVisible}>
+                <S.SubText $isVisible={isVisible}>여행 친구 관리</S.SubText>
+                <S.SubButton $isVisible={isVisible} onClick={handleMyClick}>
                     <img src={usergroup} alt="trip_plan Floating Icon" />
                 </S.SubButton>
             </S.MenuItem>
 
-            <S.MenuItem isVisible={isVisible}>
-                <S.SubText isVisible={isVisible}>여행 일정 생성</S.SubText>
-                <S.SubButton isVisible={isVisible} onClick={handleEditClick}>
+            <S.MenuItem $isVisible={isVisible}>
+                <S.SubText $isVisible={isVisible}>여행 일정 생성</S.SubText>
+                <S.SubButton $isVisible={isVisible} onClick={handleEditClick}>
                     <img src={trip_plan} alt="trip_plan Floating Icon" />
                 </S.SubButton>
             </S.MenuItem>
--- src/pages/Main/_components/FloatingButton/Styles.ts
+++ src/pages/Main/_components/FloatingButton/Styles.ts
@@ -33,20 +33,19 @@ export const FloatingContainer = styled.div`
     z-index: 1000;
 `;
 
-export const Overlay = styled.div<{ isVisible: boolean }>`
+export const Overlay = styled.div<{ $isVisible: boolean }>`
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     background: rgba(0, 0, 0, 0.8);
-    opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
-    pointer-events: ${({ isVisible }) => (isVisible ? "auto" : "none")};
+    opacity: ${({ $isVisible }) => ($isVisible ? 1 : 0)};
+    pointer-events: ${({ $isVisible }) => ($isVisible ? "auto" : "none")};
     transition: opacity 0.3s ease-in-out;
     z-index: 999;
 `;
-
-export const FloatingButtonStyled = styled.button<{ isMain?: boolean; isVisible: boolean }>`
+export const FloatingButtonStyled = styled.button<{ $isMain?: boolean; $isVisible: boolean }>`
     width: 4rem;
     height: 4rem;
     border-radius: 50%;
@@ -60,25 +59,23 @@ export const FloatingButtonStyled = styled.button<{ isMain?: boolean; isVisible:
     transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
     z-index: 1001;
 
-    ${({ isMain, isVisible }) =>
-        isMain &&
+    ${({ $isMain, $isVisible }) =>
+        $isMain &&
         css`
-            transform: ${isVisible ? "rotate(45deg)" : "rotate(0)"};
+            transform: ${$isVisible ? "rotate(45deg)" : "rotate(0)"};
         `}
 `;
-
-export const SubButton = styled(FloatingButtonStyled)<{ isVisible: boolean }>`
+export const SubButton = styled(FloatingButtonStyled)<{ $isVisible: boolean }>`
     margin-bottom: 0.5rem;
     background-color: white;
     transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
 
-    display: ${({ isVisible }) => (isVisible ? "flex" : "none")};
-    opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
-    transform: ${({ isVisible }) => (isVisible ? "scale(1)" : "scale(0)")};
+    display: ${({ $isVisible }) => ($isVisible ? "flex" : "none")};
+    opacity: ${({ $isVisible }) => ($isVisible ? 1 : 0)};
+    transform: ${({ $isVisible }) => ($isVisible ? "scale(1)" : "scale(0)")};
 `;
-
-export const MenuItem = styled.div<{ isVisible: boolean }>`
-    display: ${({ isVisible }) => (isVisible ? "flex" : "none")};
+export const MenuItem = styled.div<{ $isVisible: boolean }>`
+    display: ${({ $isVisible }) => ($isVisible ? "flex" : "none")};
     justify-content: center;
     align-items: center;
     gap: 0.75rem;
@@ -90,10 +87,10 @@ export const MenuItem = styled.div<{ isVisible: boolean }>`
     z-index: 1002;
 `;
 
-export const SubText = styled.span<{ isVisible: boolean }>`
-    display: ${({ isVisible }) => (isVisible ? "inline-block" : "none")};
-    opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
-    transform: ${({ isVisible }) => (isVisible ? "translateX(0)" : "translateX(10px)")};
+export const SubText = styled.span<{ $isVisible: boolean }>`
+    display: ${({ $isVisible }) => ($isVisible ? "inline-block" : "none")};
+    opacity: ${({ $isVisible }) => ($isVisible ? 1 : 0)};
+    transform: ${({ $isVisible }) => ($isVisible ? "translateX(0)" : "translateX(10px)")};
     transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
     white-space: nowrap;
     color: white;

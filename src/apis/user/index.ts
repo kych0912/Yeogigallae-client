8b2ca102ac0ff54b7a2bc32ea6dc85f1b32cca51
YoungCheon Kim <kyt031522@gmail.com>
2025-02-17 12:14:57 +0900

    feat: auth hoc 컴포넌트 수정 - useEffect로 data fetching 처리

--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -1,13 +1,22 @@
 import { api } from "../Axios";
 import { DEFAULT_User } from "./mocks";
 
-export const getUser = async () => {
-    if (import.meta.env.MODE === "development") {
-        return DEFAULT_User;
-    }
-    try{    
-        const response = await api.get(`/user`);
+export interface UserInfo{
+    email: string;
+    nickname: string;
+    profileImage: string;
+}
 
+interface UserResponse{
+    status: number;
+    message: string;
+    httpStatus: string;
+    result: UserInfo;
+}
+
+export const getUser = async () => {
+    try{    
+        const response = await api.get<UserResponse>(`/api/auth/user`);
         return response.data.result;
     }catch(error){
         console.error("User API 호출 오류:", error);

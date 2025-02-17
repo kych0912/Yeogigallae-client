b0d49c00bd3431e2674fa4b0dff430c944f5572a
YoungCheon Kim <kyt031522@gmail.com>
2025-02-17 17:15:34 +0900

    fix: modal 렌더링 삭제

--- src/pages/MyPage/MyFriendPage/MyFriendPage.tsx
+++ src/pages/MyPage/MyFriendPage/MyFriendPage.tsx
@@ -4,7 +4,6 @@ import { HeaderConfig } from "../../../types/header/header";
 import { useEffect } from "react";
 import Room from "./_components/Room";
 import Friend from "./_components/Friend";
-import Modal from "../../../components/Modal/core";
 
 export default function MyFriendPage() {
     const {setHeaderConfig} = useOutletContext<{setHeaderConfig: (config: HeaderConfig) => void}>();
@@ -15,7 +14,6 @@ export default function MyFriendPage() {
 
     return (
         <CommonContainer>
-            <Modal />
             <Room />
             <Friend />
         </CommonContainer>

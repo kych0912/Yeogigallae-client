faef9d7edc75c136006b22f56dc1b70abe8d0e4b
YoungCheon Kim <kyt031522@gmail.com>
2025-02-19 04:15:32 +0900

    feat: ai호출을 위한 type 지정 & api 호출 함수 생성

--- src/apis/course/index.ts
+++ src/apis/course/index.ts
@@ -1,8 +1,10 @@
-import { courseInfoMock, courseListMock } from "./mocks";
+import { courseInfoMock, courseListMock, sampleAiCourseResponse } from "./mocks";
 import { ShareCourseSchema } from "../../pages/Course/ShareCourse/schema";
 import * as z from "zod";
-import { ICourseMessageResponse } from "./types";
+import { IAiCourseIdResponse, ICourseMessageResponse, IAiCourseResponse } from "./types";
 import { api } from "../Axios";
+import { Route } from "../map/types";
+import { AiCourseToKaKaoCourse } from "../../Utils/AiCourseToKaKaoCourse";
 
 //장소 정보 타입
 export type TShareCoursePlace = z.infer<typeof ShareCourseSchema>["place"];
@@ -59,4 +61,54 @@ export const deleteCoursePlace = async (id:string, tripId:string) => {
         return null;
     }
 }
+
+export const getAiCourseId = async (tripId:string):Promise<IAiCourseIdResponse["result"]> => {
+    try{
+        const response = await api.get<IAiCourseIdResponse>(`/api/aiCourse/tripPlan/${tripId}/ids`);
+        return response.data.result;
+    }catch(error){
+        console.error("AiCourseId API 호출 오류:", error);
+        return [];
+    }
+}
+
+export const generateAiCourse = async (tripId:string) => {
+    const response = await api.post(`/api/aiCourse/tripPlan/${tripId}`);
+    return response.data.result;
+}
+
+export const getAiCourse = async (tripId:string,aiCourseId:string):Promise<IAiCourseResponse> => {
+    const response = await api.get(`/api/aiCourse/tripPlan/${tripId}/${aiCourseId}`);
+    return response.data;  
+}
+
+
+/**
+ * aiCourseId가 존재하는지 확인
+ * 확인 후 존재하면 해당 코스를 KaKao Route로 변환
+ * 없을 경우 새로운 aiCourseId를 생성
+ * aiCourseId를 생성 후 해당 코스를 KaKao Route로 변환
+ * @param tripId 여행 계획 ID
+ * @returns aiCourseId와 aiKaKaoCourse
+ */
+export const getAiKaKaoCourseAndId = async (tripId:string):Promise<{aiCourseId:string, aiKaKaoCourse:Route[]}> => {
+    const aiCourseIdArray = await getAiCourseId(tripId);
+
+    if(aiCourseIdArray.length !== 0 ){
+        const aiCourseId = aiCourseIdArray[aiCourseIdArray.length - 1].aiCourseId;
+        const aiCourse = await getAiCourse(tripId, aiCourseId);
+        
+        const aiKaKaoCourse = await AiCourseToKaKaoCourse(aiCourse);
+
+        return { aiCourseId, aiKaKaoCourse };
+    }
+
+    const aiCourseId = await generateAiCourse(tripId);
+    const aiCourse = await getAiCourse(tripId, aiCourseId);
+
+    const aiKaKaoCourse = await AiCourseToKaKaoCourse(aiCourse);
+
+    return { aiCourseId, aiKaKaoCourse };
+}
+
 

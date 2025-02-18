bf1cf5f7f37359801a201a13cc429d621ca80786
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-02-18 22:16:42 +0900

    feat:3번:aiCourseId 있는지 확인

--- src/pages/Main/_components/UpcomingItem/UpcomingItem.tsx
+++ src/pages/Main/_components/UpcomingItem/UpcomingItem.tsx
@@ -11,14 +11,20 @@ interface UpcomingItemProps {
 export default function UpcomingItem({ rooms }: UpcomingItemProps) {
     const navigate = useNavigate();
 
-    const handleClick = (roomId: number, aiCourseId: number) => {
-        navigate(`/course/upcoming/${roomId}/${aiCourseId}`);
+    const handleClick = (roomId: number, tripPlanId: number, aiCourseId: number | null) => {
+        if (aiCourseId) {
+            // aiCourseId가 존재하면, 이미 생성된 AI 코스를 렌더링
+            navigate(`/course/upcoming/${tripPlanId}/${aiCourseId}`);
+        } else {
+            // aiCourseId가 없으면, 클릭한 페이지로 이동
+            navigate(`/course/planId/${tripPlanId}/roomId/${roomId}?isEnd=true`);
+        }
     };
 
     return (
         <S.RowTravelList>
             {rooms.map((room) => (
-                <U.TravelList onClick={() => handleClick(room.aiCourseId, room.roomId)} key={room.tripPlanId}>
+                <U.TravelList onClick={() => handleClick(room.roomId, room.aiCourseId, room.tripPlanId)} key={room.tripPlanId}>
                     <U.ImageWrapper>
                         <S.Image src={room.imageUrl} alt={`${room.roomName} 이미지`} />
                     </U.ImageWrapper>

96db7e1cf7af4a2d3b869f0d0fa754833b09e1a0
YoungCheon Kim <kyt031522@gmail.com>
2025-02-19 13:17:52 +0900

    feat: 계정 삭제 & 로그아웃 구현

---
+++ src/react-query/mutation/profile/mutations.ts
@@ -0,0 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
+import { deleteAccount, logout } from "../../../apis/profile";
+import { useNavigate } from "react-router-dom";
+
+export const useLogoutAccount = () => {
+    const navigate = useNavigate();
+    return useMutation({
+        mutationFn: logout,
+        onSuccess:()=>{
+            navigate("/");
+        }
+    });
+}      
+
+export const useDeleteAccount = () => {
+    const navigate = useNavigate();
+    return useMutation({
+        mutationFn: deleteAccount,
+        onSuccess:()=>{
+            navigate("/");
+        }
+    });
+}   
+
+

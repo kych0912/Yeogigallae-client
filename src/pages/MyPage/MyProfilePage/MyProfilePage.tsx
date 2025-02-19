eb3cee737238dfbe4bcbdbcfba6217139c3e2e0d
YoungCheon Kim <kyt031522@gmail.com>
2025-02-19 13:18:04 +0900

    feat: setHeader hook으로 변경

--- src/pages/MyPage/MyProfilePage/MyProfilePage.tsx
+++ src/pages/MyPage/MyProfilePage/MyProfilePage.tsx
@@ -1,16 +1,13 @@
 import UserProfile from './_components/UserProfile';
 import ServiceSetting from './_components/ServiceSetting';
 import CommonContainer from '../../../components/Layout/CommonContainer';
-import { useOutletContext } from 'react-router-dom';
-import { HeaderConfig } from '../../../types/header/header';
-import { useEffect } from 'react';
 import { useAuthStore } from '../../Login/useAuthStore';
+import useSetHeader from '../../../hooks/useSetHeader';
+
+
 export default function MyProfilePage() {
-    const {setHeaderConfig} = useOutletContext<{setHeaderConfig: (config: HeaderConfig) => void}>();
+    useSetHeader({title:"마이페이지"});
     const { profileImage, nickname } = useAuthStore();
-    useEffect(() => {
-        setHeaderConfig({title:"마이페이지"});
-    }, []);
 
     return (
         <CommonContainer>

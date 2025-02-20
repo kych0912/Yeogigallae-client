58698cafa1c85f1653daa21fbeb6e8e98861e198
KIM JEONGMIN <wjdals030328@catholic.ac.kr>
2025-02-20 19:34:15 +0900

    fix:라우터 설정

--- src/apis/upcomingCourse/index.ts
+++ src/apis/upcomingCourse/index.ts
@@ -5,9 +5,8 @@ import { api } from "../Axios";
 export const getCourse = async (tripPlanId: string, aiCourseId: string): Promise<FirstDayCourse> => {
     try {
         const response = await api.get<CourseResponse>(`/api/aiCourse/tripPlan/${tripPlanId}/${aiCourseId}`);
-
-        // return response.data.result || null;
-        return response.data.result;
+        return courseMock.result || null;
+        // return response.data.result;
     } catch (error) {
         console.error("❌ [getCourse] API 호출 오류:", error);
         return courseMock.result || null;
